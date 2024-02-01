2650e8337a6cf58085cce048052b4517a7abce48
Lonely Sit <71547798+Lonely-Sit@users.noreply.github.com>
2024-02-01 18:44:51 +0800

    Update locale-selector.vue

--- src/modules/i18n/components/locale-selector.vue
+++ src/modules/i18n/components/locale-selector.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
 const { availableLocales, locale } = useI18n();
 
-// 如果locale为undefined，将其默认为中文
-if (!locale) {
-  // 此处使用 zh 作为默认语言，你可以根据实际情况调整
-  useI18n().locale = 'zh';
-}
-
 const localesLong: Record<string, string> = {
   zh: '中文', 
   en: 'English',
-  es: 'Español',
-  fr: 'Français',
-  pt: 'Português',
-  ru: 'Русский',
-  uk: 'Українська',
 };
 
 const localeOptions = computed(() =>
